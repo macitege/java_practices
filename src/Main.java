cc298dbb1137c2993b22c7f561073cbc4bd78acd
macitege <macitegeercan@gmail.com>
2019-04-20 21:36:15 -0400

    more practice

--- src/Main.java
+++ src/Main.java
@@ -64,16 +64,36 @@ public class Main {
 //        System.out.println("Index 2: " + Array.get(myFavoriteCandyBars, 2));
 
 
-        Dog valery = new Dog(
-                10,
-                "Lab",
-                "Female",
-                false
-        );
+//        Dog valery = new Dog(
+//                10,
+//                "Lab",
+//                "Female",
+//                false
+//        );
+//
+//        System.out.println("Age: " + valery.age);
+//        valery.yearPass(100);
+//        System.out.println("Age: " + valery.age);
+//        valery.bark();
 
-        System.out.println("Age: " + valery.age);
-        valery.yearPass(100);
-        System.out.println("Age: " + valery.age);
-        valery.bark();
+//        System.out.println("Enter your age: ");
+//        Scanner in = new Scanner(System.in);
+//        int age = in.nextInt();
+//
+//        if (age >= 0 && age <= 5) {
+//            System.out.println("Baby");
+//        } else if (age >= 6 && age <= 11) {
+//            System.out.println("Kid");
+//        } else if (age >= 12 && age <= 17) {
+//            System.out.println("Teen");
+//        } else if (age >= 18) {
+//            System.out.println("Adult");
+//        } else {
+//            System.out.println("Invalid");
+//        }
+//
+//        System.out.println("Thanks for using this program!");
+
+        
     }
 }
